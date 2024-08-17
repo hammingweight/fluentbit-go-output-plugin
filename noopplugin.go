078ee186c29d3518812230b674a75f5b7d7c412a
Carl Meijer <hammingweight@gmail.com>
2024-08-17 15:19:41 +0200

    Let the plugin do stuff.

--- noopplugin.go
+++ noopplugin.go
@@ -21,7 +21,40 @@ func FLBPluginInit(ctx unsafe.Pointer) int {
 
 //export FLBPluginFlush
 func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
-	fmt.Println("Ping")
+	dec := output.NewDecoder(data, int(length))
+	var records []map[string]string
+	for {
+		ret, ts, record := output.GetRecord(dec)
+		if ret != 0 {
+			break
+		}
+
+		fmt.Println(ts)
+		t := ts.(output.FLBTime).Time
+		fmt.Println(t)
+
+		r := make(map[string]string)
+		for k, v := range record {
+			key := k.(string)
+
+			var val string
+			switch v.(type) {
+			case []uint8:
+				val = string(v.([]uint8))
+			case string:
+				val = v.(string)
+			case nil:
+				val = ""
+			default:
+				val = fmt.Sprintf("%v", v)
+			}
+			r[key] = val
+		}
+		r["tag"] = C.GoString(tag)
+		records = append(records, r)
+		fmt.Println(records)
+	}
+
 	return output.FLB_OK
 }
 
